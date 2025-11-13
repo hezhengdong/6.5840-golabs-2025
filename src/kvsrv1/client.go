f067626c739de8708e93d778c0384c8b10404b8a
hezhengdong <2685273579@qq.com>
2025-11-13 18:53:43 +0800

    feat: Lab2: task1: 全部代码实现

--- src/kvsrv1/client.go
+++ src/kvsrv1/client.go
@@ -1,6 +1,8 @@
 package kvsrv
 
 import (
+	"log"
+
 	"6.5840/kvsrv1/rpc"
 	"6.5840/kvtest1"
 	"6.5840/tester1"
@@ -8,16 +10,25 @@ import (
 
 
 type Clerk struct {
-	clnt   *tester.Clnt
-	server string
+	clnt   *tester.Clnt // 用于发送 RPC 请求
+	server string // 目标服务器的标识
 }
 
+// 创建并返回一个新的 Clerk 实例
+// 参数:
+//   - clnt: RPC 客户端对象
+//   - server: 服务器标识符
+// 返回值: 实现了 IKVClerk 接口的 Clerk 实例，约束 Clerk 必须实现 Get Put 方法
 func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 	ck := &Clerk{clnt: clnt, server: server}
 	// You may add code here.
 	return ck
 }
 
+// Get 获取指定键的当前值和版本。
+// 如果键不存在，则返回 ErrNoKey 错误。
+// 对于其他所有错误情况，该方法会持续重试直至成功。
+//
 // Get fetches the current value and version for a key.  It returns
 // ErrNoKey if the key does not exist. It keeps trying forever in the
 // face of all other errors.
@@ -30,9 +41,25 @@ func MakeClerk(clnt *tester.Clnt, server string) kvtest.IKVClerk {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// You will have to modify this function.
-	return "", 0, rpc.ErrNoKey
+
+	args := rpc.GetArgs{Key: key}
+	reply := rpc.GetReply{}
+
+	ok := ck.clnt.Call(ck.server, "KVServer.Get", &args, &reply)
+
+	if !ok {
+		log.Printf("Get RPC 调用失败. key: %v\n", key)
+		return "", 0, rpc.ErrNoKey
+	}
+
+	return reply.Value, reply.Version, reply.Err
 }
 
+// Put 仅在版本号匹配时更新 key 和 value。
+// 如果版本号不匹配，服务器应当返回 ErrVersion。
+// 如果 Put 在第一次 RPC 请求时接收到 ErrVersion，Put 应当返回 ErrVersion，因为 Put 一定不是在服务器上被执行。
+// 如果服务器在重发 RPC 时返回 ErrVersion，Put 必须向应用层返回 ErrMaybe，因为先前的 RPC 可能被服务器成功处理，但是响应丢失，Clerk 无法确定 Put 是否被执行。
+//
 // Put updates key with value only if the version in the
 // request matches the version of the key at the server.  If the
 // versions numbers don't match, the server should return
@@ -52,5 +79,20 @@ func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 // arguments. Additionally, reply must be passed as a pointer.
 func (ck *Clerk) Put(key, value string, version rpc.Tversion) rpc.Err {
 	// You will have to modify this function.
-	return rpc.ErrNoKey
+
+	args := rpc.PutArgs{
+		Key: key,
+		Value: value,
+		Version: version,
+	}
+	reply := rpc.PutReply{}
+
+	ok := ck.clnt.Call(ck.server, "KVServer.Put", &args, &reply)
+
+	if !ok {
+		log.Printf("Put RPC 调用失败. key: %v, value: %v, version: %v", key, value, version)
+		return rpc.ErrNoKey
+	}
+
+	return reply.Err
 }
--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -18,31 +18,81 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type DataValue struct {
+	Value string
+	Version rpc.Tversion
+}
 
 type KVServer struct {
 	mu sync.Mutex
 
 	// Your definitions here.
+	Data map[string]DataValue
 }
 
 func MakeKVServer() *KVServer {
 	kv := &KVServer{}
 	// Your code here.
+	kv.Data = make(map[string]DataValue)
 	return kv
 }
 
+// 如果 args.Key 存在，Get 返回 args.Key 的 value 和 version。反之，Get 返回 ErrNoKey。
+//
 // Get returns the value and version for args.Key, if args.Key
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here.
+	kv.mu.Lock() // 读操作也需要加锁，只有读读并发没问题，读写并发、写写并发有问题。可以使用读写锁优化
+	dataValue, exist := kv.Data[args.Key] // 只对共享资源加锁
+	kv.mu.Unlock()
+
+	if exist {
+		reply.Value = dataValue.Value
+		reply.Version = dataValue.Version
+		reply.Err = rpc.OK
+	} else {
+		reply.Err = rpc.ErrNoKey
+	}
 }
 
+// 如果版本匹配，更新 key 的 value。
+// 如果版本不匹配，返回 ErrVersion。
+// 如果 key 不存在，并且 args.Version 为 0，Put 安装 value，否则返回 ErrNoKey。
+//
 // Update the value for a key if args.Version matches the version of
 // the key on the server. If versions don't match, return ErrVersion.
 // If the key doesn't exist, Put installs the value if the
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	dataValue, exist := kv.Data[args.Key]
+	if exist {
+		if args.Version == dataValue.Version { // key 存在 && 版本匹配
+			args.Version++
+			kv.Data[args.Key] = DataValue{
+				Value: args.Value,
+				Version: args.Version,
+			}
+			reply.Err = rpc.OK
+		} else { // key 存在 && 版本不匹配
+			reply.Err = rpc.ErrVersion
+		}
+	} else {
+		if args.Version == 0 { // key 不存在 && 版本为零
+			args.Version++
+			kv.Data[args.Key] = DataValue{
+				Value: args.Value,
+				Version: args.Version,
+			}
+			reply.Err = rpc.OK
+		} else { // key 不存在 && 版本不为零
+			reply.Err = rpc.ErrNoKey
+		}
+	}
 }
 
 // You can ignore Kill() for this lab
