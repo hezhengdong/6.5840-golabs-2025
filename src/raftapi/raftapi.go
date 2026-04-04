8f5a796c50d1a84f39490b8bd37cd1b4c7d25647
hezhengdong <2685273579@qq.com>
2026-04-04 15:48:33 +0800

    docs: raftapi.go 中文注释

--- src/raftapi/raftapi.go
+++ src/raftapi/raftapi.go
@@ -1,6 +1,7 @@
 package raftapi
 
 // The Raft interface
+// 该接口定义了 Raft 的核心方法
 type Raft interface {
 	// Start agreement on a new log entry, and return the log index
 	// for that entry, the term, and whether the peer is the leader.
@@ -28,12 +29,12 @@ type Raft interface {
 // snapshots) on the applyCh; at that point you can add fields to
 // ApplyMsg, but set CommandValid to false for these other uses.
 type ApplyMsg struct {
-	CommandValid bool
-	Command      interface{}
-	CommandIndex int
+	CommandValid bool        // 为 true 表示包含新提交的日志条目
+	Command      interface{} // 提交的命令内容
+	CommandIndex int         // 命令在日志中的索引位置
 
-	SnapshotValid bool
-	Snapshot      []byte
-	SnapshotTerm  int
-	SnapshotIndex int
+	SnapshotValid bool   // 为 true 表示包含快照数据
+	Snapshot      []byte // 快照数据字节数组
+	SnapshotTerm  int    // 快照对应的任期
+	SnapshotIndex int    // 快照对应的日志索引
 }
